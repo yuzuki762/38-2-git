52c22f21f3b8c509f7369d3a017855c4902c200e
Yuzuki Oomura <184710903+yuzuki762@users.noreply.github.com>
2025-05-26 15:28:20 +0900

    色を紫に変更しました

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'
+    'purple', 'purple', 'purple', 'purple', 'purple', 'purple', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
